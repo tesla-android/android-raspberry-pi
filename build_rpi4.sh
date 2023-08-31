73e7733bb0f2d86e4f994e8f06105040b9d5aac5
Michał Gapiński <mike@gapinski.eu>
2023-08-31 21:51:58 +0200

    Add CM4 target

--- build_rpi4.sh
+++ build_rpi4.sh
@@ -5,10 +5,6 @@ trap 'echo -e "\nbuild.sh interrupted"; exit 1' SIGINT
 echo Building the Android
 pushd aosptree
 . build/envsetup.sh
-lunch tesla_android_cm4-userdebug
-make images -k || make images -j1
-make sdcard
-make otapackage
 lunch tesla_android_rpi4-userdebug
 make images -k || make images -j1
 make sdcard
